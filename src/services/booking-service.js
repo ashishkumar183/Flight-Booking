8d1bef363e7b9491d229c9c1da44c754d3af8a26
Ashish269 <ashishkumar183183@gmail.com>
2026-03-16 12:57:28 +0530

    FEAT: Added booking logic with transaction

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,32 +1,63 @@
 const axios = require('axios');
-const db = require('../models');
+const { StatusCodes } = require('http-status-codes');
+
 const { BookingRepository } = require('../repositories');
 const { ServerConfig } = require('../config');
-const { StatusCodes } = require('http-status-codes');
+const db = require('../models');
 const AppError = require('../utils/errors/app-error');
 
+const bookingRepository = new BookingRepository();
+
+
 async function createBooking(data) {
-    console.log("Calling seat update API...");
-    console.log("Sending params:", {
-    seats: data.noofSeats,
-    dec: 1
-});
-    await axios.patch(
-  `${ServerConfig.FLIGHT_API_URL}/api/v1/flights/${data.flightId}/seats`,
-  {
-    seats: data.noofSeats,
-    dec: 1
-  }
-);
+    console.log("Incoming data:", data);
+    const transaction = await db.sequelize.transaction();
 
-    return {
-        flightId: data.flightId,
-        seatsBooked: data.noofSeats,
-        status: "CONFIRMED"
-    };
+    try {
+        const flight = await axios.get(
+            `${ServerConfig.FLIGHT_API_URL}/api/v1/flights/${data.flightId}`
+        );
+
+        const flightData = flight.data.data;
+
+        if (data.noofSeats > flightData.totalSeats) {
+            throw new AppError(
+                'Not enough seats available',
+                StatusCodes.BAD_REQUEST
+            );
+        }
+
+        const totalBillingAmount = data.noofSeats * flightData.price;
+
+        const bookingPayLoad = {
+            flightId: data.flightId,
+            userId: data.userId,
+            noOfSeats: data.noofSeats,
+            totalCost: totalBillingAmount
+        };
+
+        const booking = await bookingRepository.createBooking(
+            bookingPayLoad,
+            transaction
+        );
+
+        await axios.patch(
+            `${ServerConfig.FLIGHT_API_URL}/api/v1/flights/${data.flightId}/seats`,
+            {
+                seats: data.noofSeats,
+                dec: 1
+            }
+        );
+
+        await transaction.commit();
+
+        return booking;
+
+    } catch (error) {
+        await transaction.rollback();
+        throw error;
+    }
 }
-
-
 
 module.exports = {
     createBooking
